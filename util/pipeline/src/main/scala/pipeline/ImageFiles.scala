0964d15d59bf97c7d82f320b1f8674b67764019d
Kyle Unverferth <kyle@kyleu.com>
2018-07-14 15:27:33 -0600

    Hud images

--- util/pipeline/src/main/scala/pipeline/ImageFiles.scala
+++ util/pipeline/src/main/scala/pipeline/ImageFiles.scala
@@ -2,13 +2,16 @@ package pipeline
 
 object ImageFiles {
   def process(cfg: PipelineConfig) = Seq(
+    cfg.copyAsset("images/sparkle.png", "images/sparkle.png"),
     cfg.copyAsset("images/menu/splash.png", "images/splash.png"),
     cfg.copyAsset("images/scanning/scanningbar.png", "images/progress.png"),
     cfg.copyAsset("images/menu/logo.png", "images/logo.png"),
     cfg.copyAsset("images/menu/cityscape.png", "images/mainbg.png"),
+
     cfg.copyAsset("images/characters", "images/character"),
     cfg.copyAsset("images/scanning", "images/intro"),
-    cfg.copyAsset("images/tilesets", "images/tileset"),
-    cfg.copyAsset("images/sprites", "images/sprites")
+    cfg.copyAsset("images/hud", "images/hud"),
+    cfg.copyAsset("images/sprites", "images/sprites"),
+    cfg.copyAsset("images/tilesets", "images/tileset")
   ).flatten
 }
